a90208b13a20b222497c0f90de4f529cdf00a3b0
Alexis Ramírez <89491557+aleexisrmz@users.noreply.github.com>
2023-05-31 12:48:22 -0600

    Add files via upload

---
+++ express/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const {infoCursos} = require('./datos/cursos.js');
+
+// ROUTERS
+
+const routerProgramacion = require('./routers/programacion.js');
+app.use('/api/cursos/programacion', routerProgramacion);
+
+const routerMatematicas = require('./routers/matematicas.js');
+app.use('/api/cursos/matematicas', routerMatematicas);
+
+//
+
+app.get('/', (req, res)=>{
+    res.send("Mi primer servidor con Express");
+});
+
+app.get('/api/cursos', (req, res)=>{
+    res.send(JSON.stringify(infoCursos));
+});
+
+const PUERTO = process.env.PORT || 3000;
+
+app.listen(PUERTO, ()=>{
+    console.log("El servidor está iniciado");    
+});
